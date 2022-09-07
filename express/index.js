496b1008c93baf96a72b665cfc7c271d24099463
jpdealba <yoda9@live.com.mx>
2022-09-07 10:10:58 -0500

    handlebars implementation to express

--- express/index.js
+++ express/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const rutasNoticias = require("./noticias/routes");
+const { engine } = require("express-handlebars");
+
 // Inicializar app
 const app = express();
 const path = require("path");
+
+//registrar engine
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+
 app.use("/assets", express.static(path.join(__dirname, "public")));
 
 const middleWare = (req, res, next) => {
@@ -11,7 +18,12 @@ const middleWare = (req, res, next) => {
 };
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  //res.sendFile(path.join(__dirname, "/index.html"));
+  res.render("index", {
+    title: "Prueba desde index.js",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+  });
 });
 
 app.listen(3000, () => {
